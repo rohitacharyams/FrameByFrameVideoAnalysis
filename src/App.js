d22e44ef280f159418bec7c5992e6159db9829e6
ROHIT ACHARYA <rohitacharya@ROHITs-MacBook-Pro.local>
2024-04-20 00:16:35 +0530

    Added style to adjust height of video player

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
           <Route path="/review" element={<ReviewPage />} />
         </Routes>
       </PlayerContext.Provider>
-      
+      <FileInput />
     </Router>
   );
 };
